14d37859c945caecc463347f0b70404efc706f1f
pengsheng.zhu <1137846786@qq.com>
2021-09-06 14:34:33 +0800

    init

---
+++ netease-cloud-music/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<style scoped>
+
+</style>
